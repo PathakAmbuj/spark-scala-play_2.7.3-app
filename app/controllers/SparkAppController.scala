c210f407d455cd81f4f57f2819aa09f9519de576
Ambuj Pathak <34556769+PathakAmbuj@users.noreply.github.com>
2019-11-06 22:37:51 +0530

    Update SparkAppController.scala

--- app/controllers/SparkAppController.scala
+++ app/controllers/SparkAppController.scala
@@ -7,6 +7,7 @@ import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.types.{DoubleType, IntegerType, StringType, StructField, StructType}
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 import views.html.textToHtml
+import views.html.createFormattedTable
 
 import scala.concurrent.Future
 
@@ -40,7 +41,7 @@ class SparkAppController @Inject()(cc: ControllerComponents)(implicit assetsFind
         // .addAppArgs()
         .startApplication()
 
-      Future.successful(Ok(" Job Submitted Successfully ..........."))
+      Future.successful(Ok(textToHtml.render("<h2> Job Submitted Successfully ...........</h2>"))
     }
   }
 
@@ -78,33 +79,13 @@ class SparkAppController @Inject()(cc: ControllerComponents)(implicit assetsFind
       }
       val res = ds.select(cols.head, cols.tail: _*)
 
-      val sb = new StringBuilder
-      val htmlHeader: String = "<!DOCTYPE html><html><head><style>table tbody, table thead{ display: block;}table tbody " +
-        "{   overflow: auto;   height: 500px;} table thead {\n\t\n\tbackground:#f9f9fe;\n}\nth\n{\n\theight : 50px;\t\n\t" +
-        "width : 150px;\n\ttext-align : left;\n\tborder: 1px solid black;\"\t\n}\ntd\n" +
-        "{\n    width : 150px;\n    text-align: left;\n\tborder: 1px solid black;\"\n}\n</style>\n" +
-        "</head>\n<body>\n\n<h2>HTML Table</h2>\n<table>\n<thead>\n<tr>"
-      sb.append(htmlHeader)
+     
       val schema = res.schema
       val scm = schema.fieldNames
-      for (x <- scm) {
-        sb.append("<th>" + x + "</th>")
-      }
-      sb.append("</tr></thead><tbody>")
-
-
+      
       val list = res.limit(row).collect.toList
-      for (l <- list) {
-        sb.append("<tr>")
-        for (x <- scm.indices) {
-          sb.append("<td>" + l.get(x) + "</td>")
-        }
-        sb.append("</tr>")
-      }
-
-      sb.append("</tbody></table></body>\n" + "</html>")
-
-      Future.successful(Ok(textToHtml.render(sb.toString())))
+      
+      Future.successful(Ok(createFormattedTable.render(scm, list)))
 
     }
   }
